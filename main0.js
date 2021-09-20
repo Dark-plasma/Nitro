a1a3c73924dfefede8a53864368aedf8aa10a3c2
Dark-plasma <86374151+Dark-plasma@users.noreply.github.com>
2021-09-20 18:20:17 +0530

    Add files via upload

---
+++ main0.js
@@ -0,0 +1,34 @@
+(function(){
+    var oldLog = console.log;
+    console.log = function (message) {
+        document.getElementById('a').value += message
+        oldLog.apply(arguments);
+    };
+})();
+
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+var textarea = document.getElementById('a')
+if (navigator.userAgent.indexOf('Mobile') !== -1) { document.getElementById('save').style.display = "none"; textarea.style.height = "100px";
+}//resize for mobile
+getGiftCode = function () {
+    let code = '';
+    let dict = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    for(var i = 0; i < 16; i++){
+        code = code + dict.charAt(Math.floor(Math.random() * dict.length));
+    }
+    console.log('[GEN] ' + code + "\n");
+    code += '\n'
+    document.getElementById('b').value += code;
+
+} //generates codes
